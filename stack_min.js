09b10bbc7308f16fb9032229bc3c2b9c8a46c222
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-11-20 08:12:24 +0800

    Implement stack_min on ruby

--- stack_min.js
+++ stack_min.js
@@ -10,25 +10,21 @@ class Stack {
   push(data) {
     this.list.push(data);
     this.least === null || this.minList.head === null || data < this.least ? (this.minList.commit(data), this.least = data) : undefined;
+    return this.least;
   }
   pop() {
     const popped = this.list.pop();
-    popped === this.minList.head.data ? this.minList.reset() : undefined;
+    popped === this.minList.head.data ? (this.minList.reset(), this.least = this.minList.head === null ? null : this.minList.head.data) : undefined;
   }
   min() {
     return this.minList.head.data;
   }
 }
 
-const stack = new Stack,
-  input = [3, 5, -1, -1, 2, 7, 11, -1, -1];
+const inputMin = [3, 5, -1, -1, 7, 2, 11, -1, -1, 9];
 
-function do_stuff(arr) {
-  let [temp, output] = [
-    [], ""
-  ];
-
-  arr.forEach(v => v === -1 ? output += " " + temp.pop() : temp.push(v));
-
+function do_stuff_min(arr) {
+  let [stack, output] = [new Stack, ""];
+  arr.forEach(v => v === -1 ? stack.pop() : output += stack.push(v) + " ");
   return output;
 }
