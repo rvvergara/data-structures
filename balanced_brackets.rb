42264e0e116d3f788a658eee6063af0f79ddc78c
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-11-22 09:21:49 +0800

    Add descriptive comments on ruby version to explain steps

--- balanced_brackets.rb
+++ balanced_brackets.rb
@@ -2,15 +2,22 @@
 # Array implementation to make solution shorter
 
 def do_stuff(text)
+  # 1. Declare an array called unpaired
   unpaired = Array.new
-  text.chars.each do |val|
-    if tags.keys.include? val
-      unpaired.push(val)
-    elsif tags.values.include? val
-      tags[unpaired.pop] == val ? nil : unpaired.push(val)
+  # 2. Traverse through text characters
+  text.chars.each do |char|
+  # 3. If char is a key in tags then push it to unpaired
+    if tags.keys.include? char
+      unpaired.push(char)
+  # 4. Else if char is a value in tags (char is a closing tag) then
+    elsif tags.values.include? char
+  # 5. Pop unpaired and determine if this popped value is an openning tag of char
+  # 6. If it is then do nothing otherwise push char into unpaired
+      if tags[unpaired.pop] != char then unpaired.push(char); end
     end
   end
-  unpaired.size == 0
+  # 7. After all the text is traversed then check whether or not unpaired's length is 0
+  puts unpaired.size == 0
 end
 
 def tags
@@ -22,4 +29,15 @@ def tags
   }
 end
 
-puts do_stuff("()[]([{[[[{{()}}]]]}][]()")
+inputs = ["{}(hello)[world]",
+  "[({}{}{})([])]",
+  "([)]",
+  "(hi))(lo)(",
+  "()[]([{[[[{{()}}]]]}])[]()",
+  "[({}{}{)}([])]",
+  "()[]([{[[[{{()}}]]]}][]()",
+  "[({}[])[]()[{()]}]",
+  "[{([](){}[](){})}]"
+]
+
+inputs.each {|input| do_stuff(input)}
