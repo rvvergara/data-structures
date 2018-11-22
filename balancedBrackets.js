9d154d3da7c39282de559d0453ed1077ddb2a9c9
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-11-22 19:16:16 +0800

    Add input on balancedBrackets.js

--- balancedBrackets.js
+++ balancedBrackets.js
@@ -12,8 +12,21 @@ function do_stuff(text) {
   text.split("").forEach(v => {
     if (Object.keys(tags()).includes(v)) unpaired.push(v)
     else if (Object.values(tags()).includes(v)) {
-      tags[unpaired.pop()] === v ? undefined : unpaired.push(v)
+      tags()[unpaired.pop()] === v ? undefined : unpaired.push(v)
     }
   })
-  return unpaired.length === 0
+  console.log(unpaired.length === 0)
 }
+
+const inputs = ["{}(hello)[world]",
+  "[({}{}{})([])]",
+  "([)]",
+  "(hi))(lo)(",
+  "()[]([{[[[{{()}}]]]}])[]()",
+  "[({}{}{)}([])]",
+  "()[]([{[[[{{()}}]]]}][]()",
+  "[({}[])[]()[{()]}]",
+  "[{([](){}[](){})}]"
+];
+
+inputs.forEach(input => do_stuff(input))
