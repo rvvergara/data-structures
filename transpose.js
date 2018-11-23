0e5ca610817d9028553f6dad2bab479d8f305cb4
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-11-23 09:53:30 +0800

    Create input for transpose.js

--- transpose.js
+++ transpose.js
@@ -1,18 +1,29 @@
-function do_stuff(text){
-  const [stack, temp] = [[],[]];
+function do_stuff(text) {
+  const [stack, temp] = [
+    [],
+    []
+  ];
   text.split("").forEach(v => {
-    if(v === "n"){
-      while(stack[stack.length - 1] === "g"){
+    if (v === "n") {
+      while (stack[stack.length - 1] === "g") {
         temp.push(stack.pop());
       }
       stack.push(v);
-      while(temp.length !== 0){
+      while (temp.length !== 0) {
         stack.push(temp.pop())
       }
-    }else{
+    } else {
       stack.push(v)
     }
   })
   return stack.join("");
 }
 
+texts = ["he was searchign on Bign for signign kigns",
+  "rignadingdiggn!",
+  "gngngnnggnngggnnn",
+  "pign me when you see ggn",
+  "ggggggnnnnnn"
+]
+
+texts.forEach(text => console.log(do_stuff(text)))
