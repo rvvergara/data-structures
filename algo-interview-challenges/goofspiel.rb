afff8230486fc5d5e7a42e8f948c408de74e7d1c
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-03-27 10:19:21 +0800

    Finish goofspiel challenge

--- algo-interview-challenges/goofspiel.rb
+++ algo-interview-challenges/goofspiel.rb
@@ -1,49 +1,23 @@
 class Player
-  attr_reader :hand, :score
+  attr_accessor :hand
   def initialize(args)
     @hand = args[:hand]
-    @score = 0
   end
-
-  def bid(card)
-    card = hand.cards[card]
-    bid = card.value
-    bid
-  end
-
-  def add_score(bid, value)
-    @score += value
-    hand.cards.delete(bid)
-  end
-
   
+  def bid(card)
+    index = hand.cards.index(card)
+    hand.cards = hand.cards[0, index] + hand.cards[index + 1, hand.cards.size - index]
+  end
+
+  def score
+    hand.cards.reduce {|a, b| a + b}
+  end
 end
 
 class Hand
-  attr_reader :cards
-  def initialize(args)
-    @cards = {
-      1=>  args[:card1],
-      2=>  args[:card2],
-      3=>  args[:card3],
-      4=>  args[:card4],
-      5=>  args[:card5],
-      6=>  args[:card6],
-      7=>  args[:card7],
-      8=>  args[:card8],
-      9=>  args[:card9],
-      10=> args[:card10],
-      11=> args[:card11],
-      12=> args[:card12],
-      13=> args[:card13],
-    }
-  end
-end
-
-class Card
-  attr_reader :value
-  def initialize(value)
-    @value = value
+  attr_accessor :cards
+  def initialize
+    @cards = (1..13).to_a
   end
 end
 
@@ -56,65 +30,74 @@ class Game
   end
 
   def turn(face, bid1, bid2)
-    @player1.bid(bid1)
-    @player2.bid(bid2)
-    compare(face, bid1, bid2)
-    @deck.cards.delete(face) if bid1 != bid2
+    index = deck.cards.index(face)
+    if bid1 != bid2
+      deck.cards = deck.cards[0, index] + deck.cards[index + 1, deck.cards.size - index]
+      player1.bid(bid1)
+      player2.bid(bid2)
+      compare_and_award(bid1, bid2, face)
+    end
+  end
+
+  def cycle(game_ar)
+    face = 1
+    game_ar.each do |game_turn|
+      turn(face, game_turn[0], game_turn[1])
+      face += 1 if game_turn[0] != game_turn[1]
+    end
+    [player1.score, player2.score].join(" ")
   end
 
   private
 
-  def compare(face, bid1, bid2)
-    if(bid1 > bid2)
-      @player1.add_score(bid1, face)
+  def compare_and_award(bid1, bid2, face)
+    if bid1 > bid2
+      player1.hand.cards.push(face)
+      player1.hand.cards.sort!
     elsif bid2 > bid1
-      @player2.add_score(bid2, face)
+      player2.hand.cards.push(face)
+      player2.hand.cards.sort!
     end
   end
 end
 
-cards = {
-  :card1 => Card.new(1),
-  :card2 => Card.new(2),
-  :card3 => Card.new(3),
-  :card4 => Card.new(4),
-  :card5 => Card.new(5),
-  :card6 => Card.new(6),
-  :card7 => Card.new(7),
-  :card8 => Card.new(8),
-  :card9 => Card.new(9),
-  :card10 => Card.new(10),
-  :card11 => Card.new(11),
-  :card12 => Card.new(12),
-  :card13 => Card.new(13)
-}
+def do_stuff(arr, game_args)
+  game_class = game_args[:game_class]
+  player_class = game_args[:player_class]
+  hand_class = game_args[:hand_class]
+  arr.each do |input|
+    player1 = player_class.new({hand: hand_class.new})
+    player2 = player_class.new({hand: hand_class.new})
+    game = game_class.new({deck: hand_class.new, player1: player1, player2: player2})
+    puts game.cycle(process_inputs(input))
+  end
+end
 
-player1 = Player.new({hand: Hand.new(cards)})
-player2 = Player.new({hand: Hand.new(cards)})
+def process_inputs(arr)
+  pairs = []
+  pair = []
+  until arr.empty?
+    if pair.size == 2
+      pairs.push(pair)
+      pair = []
+    else
+      pair.push(arr.shift)
+    end
+  end
+  pairs.push(pair)
+  pairs
+end
 
-game = Game.new({deck: Hand.new(cards), player1: player1, player2: player2})
+player1 = Player.new({hand: Hand.new})
+player2 = Player.new({hand: Hand.new})
 
-game.turn(1, 1, 2)
-game.turn(2, 2, 3)
-game.turn(3, 3, 4)
-game.turn(4, 4, 5)
-game.turn(5, 5, 6)
-game.turn(6, 6, 7)
-game.turn(7, 7, 8)
-game.turn(8, 8, 9)
-game.turn(9, 9, 10)
-game.turn(10, 10, 11)
-game.turn(11, 11, 12)
-game.turn(12, 12, 13)
-game.turn(13, 13, 1)
-
-puts game.player1.score
-puts game.player2.score
+game = Game.new({deck: Hand.new, player1: player1, player2: player2})
 
 inputs = [
-  '1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 11 11 12 12 13 13 1'
+  '1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 11 11 12 12 13 13 1',
+  '1 2 4 3 5 1 2 6 9 7 10 1 2 11 11 4 13 12 3 10 3 7 12 13 8 12',
+  '2 1 3 2 4 3 5 5 6 4 1 5 7 6 8 7 9 8 10 9 11 11 12 12 13 13 1 10 12 11 13 12 5 13',
+  '1 2 4 3 5 1 2 6 9 7 8 8 10 1 13 13 12 12 2 11 13 13 12 12 11 4 13 12 3 10 9 7 12 13 8 12'
 ].map {|input| input.split.map{|s| s.to_i}}
 
-puts
-print inputs[0]
-puts
+do_stuff(inputs, {game_class: Game, player_class: Player, hand_class: Hand})
